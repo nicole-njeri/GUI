1ab83aa36757771fa796f771642b92464134764d
nicole-njeri <nicole.njeri@strathmore.edu>
2024-09-25 14:48:15 +0300

    index page

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//require_once "load.php";
+//print $Obj->user_age("Alex", 2004);
+require "load.php";
+$ObjLayouts->heading();
+$ObjMenus->main_menu();
+$ObjHeadings-> main_banner();
+$ObjCont->main_content();
+$ObjCont->side_bar();
+$ObjLayouts->footer();
+?>
