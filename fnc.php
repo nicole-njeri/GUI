0c7d715ec8eb7e1a175ce375fe3c5ef2dfe1779d
nicole-njeri <nicole.njeri@strathmore.edu>
2024-09-25 15:03:13 +0300

    fnc

---
+++ fnc.php
@@ -0,0 +1,19 @@
+<?php
+
+class fnc{
+    var $fname;
+    public $username;
+    protected $email_address;
+    private $password;
+
+    public function computer_user($fname){
+        return $fname;
+    }
+
+    public function user_age($name, $yob){
+        $age = date('Y') - $yob;
+        return $name . " is " . $age;
+    }
+}
+
+?>
