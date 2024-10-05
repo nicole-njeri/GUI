972e07122bf1f939ad28fab65cdb94b7a6ca5470
nicole-njeri <nicole.njeri@strathmore.edu>
2024-10-05 19:59:21 +0300

    About main

--- about.php
+++ about.php
@@ -1,11 +1,8 @@
 <?php
-//require_once "load.php";
-//print $Obj->user_age("Alex", 2004);
 require "load.php";
 $ObjLayouts->heading();
 $ObjMenus->main_menu();
-$ObjHeadings-> main_banner();
+$ObjHeadings->main_banner();
 $ObjCont->about_content();
 $ObjCont->side_bar();
 $ObjLayouts->footer();
-?>
