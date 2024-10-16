c5d0d51be53f8ef82f79ec0d6f5e2208ee0b67da
nicole-njeri <nicole.njeri@strathmore.edu>
2024-10-16 15:23:31 +0300

    require changes

--- load.php
+++ load.php
@@ -2,39 +2,41 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+
 require "includes/constants.php";
 require "includes/dbConnection.php";
 require "lang/en.php";
 
-
+// Autoload classes from specified directories
 function classAutoLoad($classname){
-
     $directories = ["contents", "layouts", "menus", "forms", "processes", "global"];
-
-    foreach($directories AS $dir){
+    foreach($directories as $dir){
         $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
-        if(file_exists($filename) AND is_readable($filename)){
+        if(file_exists($filename) && is_readable($filename)){
             require_once $filename;
         }
     }
 }
 
-    spl_autoload_register('classAutoLoad');
+spl_autoload_register('classAutoLoad');
 
-    $ObjGlob = new fncs();
-    $ObjSendMail = new SendMail();
+// Create instances of all required classes
+$ObjGlob = new fncs();
+$ObjSendMail = new SendMail();
+$ObjLayouts = new layouts();
+$ObjMenus = new menus();
+$ObjHeadings = new headings();
+$ObjCont = new contents();
+$ObjForm = new user_forms();
 
-// Create instances of all classes
-    $ObjLayouts = new layouts();
-    $ObjMenus = new menus();
-    $ObjHeadings = new headings();
-    $ObjCont = new contents();
-    $ObjForm = new user_forms();
+// Establish database connection using constants
+try {
     $conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
+} catch (Exception $e) {
+    die("Error: Could not connect to the database. " . $e->getMessage());
+}
 
-// Create process instances
-
-    $ObjAuth = new auth();
-    $ObjAuth->signup($conn, $ObjGlob, $ObjSendMail, $lang, $conf);
-    $ObjAuth->verify_code($conn, $ObjGlob, $ObjSendMail, $lang, $conf);
-
+// Create process instances and call methods
+$ObjAuth = new auth();
+$ObjAuth->signup($conn, $ObjGlob, $ObjSendMail, $lang, $conf);
+$ObjAuth->verify_code($conn, $ObjGlob, $ObjSendMail, $lang, $conf);
